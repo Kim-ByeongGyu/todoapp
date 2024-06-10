0e30dac89af1edbed2c873cf963f07c7fb5eee16
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-06-10 15:37:50 +0900

    first commit

---
+++ src/main/java/org/example/todoapp/controller/UserController.java
@@ -0,0 +1,35 @@
+package org.example.todoapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.todoapp.entity.User;
+import org.example.todoapp.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+    }
+}
